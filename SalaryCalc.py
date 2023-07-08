456f33f5d0200874b2ffc635a6eb9f3e85c51969
Monish Prasad V <monishprasad2018@gmail.com>
2023-07-08 22:24:39 +0530

    Initial commit

---
+++ SalaryCalc.py
@@ -0,0 +1,17 @@
+workingHours = []
+salary = 0
+
+for i in range(7):
+    workingHours.append(int(input()))
+    salary += (workingHours[i] * 100)
+
+    if workingHours[i] > 8:
+        salary += (workingHours[i] % 8) * 15
+
+if workingHours[0]:
+    salary += (workingHours[0] * 100) // 2
+
+if workingHours[6]:
+    salary += (workingHours[6] * 100) // 4
+
+print(int(salary))
