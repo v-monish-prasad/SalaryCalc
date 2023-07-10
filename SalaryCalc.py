adcdf82722a21e9efd95cc8ef270e56c86c17ef4
Monish Prasad V <monishprasad2018@gmail.com>
2023-07-10 09:39:52 +0530

    Logic for more than 40 hrs added

--- SalaryCalc.py
+++ SalaryCalc.py
@@ -6,12 +6,15 @@ for i in range(7):
     salary += (workingHours[i] * 100)
 
     if workingHours[i] > 8:
-        salary += (workingHours[i] % 8) * 15
+        salary += (workingHours[i] - 8) * 15
 
 if workingHours[0]:
     salary += (workingHours[0] * 100) // 2
 
 if workingHours[6]:
     salary += (workingHours[6] * 100) // 4
+
+if sum(workingHours) > 40:
+    salary += (sum(workingHours) - 40) * 25
 
 print(int(salary))
